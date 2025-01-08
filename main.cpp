518996d2ee369f906e3c7fbd8c91ec63286ddab2
Clay Shippy <teslaenergy@gmail.com>
2025-01-08 00:30:23 -0700

    quickfix

--- main.cpp
+++ main.cpp
@@ -62,12 +62,12 @@ private slots:
         QTime currentTime = QTime::currentTime();
         clockLabel->setText(currentTime.toString("hh:mm:ss ap"));
 
-        if (currentTime.minute() == 0 && currentTime.second() <= 3) {
+        if (currentTime.minute() == 0 && currentTime.second() <= 5) {
             int hour12 = currentTime.hour() % 12;
             if (hour12 == 0) hour12 = 12; // Convert 0 to 12 for 12-hour format
             playCuckoo(hour12);
              bplay=true;
-        }else if (halfHourChimeCheckBox->isChecked() && currentTime.minute() == 30 && currentTime.second() <= 3) {
+        }else if (halfHourChimeCheckBox->isChecked() && currentTime.minute() == 30 && currentTime.second() <= 5) {
             playCuckoo(1); // Play one cuckoo sound for the half-hour chime
             bplay=true;
         }       else
@@ -75,10 +75,10 @@ private slots:
             bplay=false;
         }
 
-        if ( currentTime.minute() == 55 && currentTime.second() <= 3) {
+        if ( currentTime.minute() == 55 && currentTime.second() <= 5) {
            trayIcon->setIcon(QIcon("/Applications/cuckooClock.app/Contents/MacOS/Icon2.png"));
         }
-        if (halfHourChimeCheckBox->isChecked() && currentTime.minute() == 25 && currentTime.second() <= 3) {
+        if (halfHourChimeCheckBox->isChecked() && currentTime.minute() == 25 && currentTime.second() <= 5) {
             trayIcon->setIcon(QIcon("/Applications/cuckooClock.app/Contents/MacOS/Icon2.png"));
          }
 
